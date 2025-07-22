76ec439b8d82f89872ce7c7faf05a5438280d261
neginheidarii <neginheidari@icloud.com>
2025-07-22 15:54:18 -0400

    Track frontend folder properly

---
+++ frontend/src/services/fetchPost.ts
@@ -0,0 +1,30 @@
+import {request, gql} from "graphql-request"
+
+const fetchPost = async() => {
+
+    const endpoint = "http://localhost:4000/graphql";
+
+    const getPostQuery =
+
+    gql`
+        {
+        posts {
+            id
+            title
+            content
+            category {
+            id
+            name
+            }
+        }
+        }
+    ` 
+
+      const data = await request(endpoint, getPostQuery);
+     return data;
+
+
+
+}
+
+export default fetchPost;
