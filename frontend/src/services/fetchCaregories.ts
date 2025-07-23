4d30b4c947157feca624c5e018078227b521beac
neginheidarii <neginheidari@icloud.com>
2025-07-23 16:21:55 -0400

    chore: add fetchCaregories service file with commented-out GraphQL query

---
+++ frontend/src/services/fetchCaregories.ts
@@ -0,0 +1,12 @@
+// import { request, gql } from "graphql-request";
+
+// const endpoint = "http://localhost:4000/graphql";
+
+// const categoryQuery = gql`
+//   query {
+//     categories {
+//       id
+//       name
+//     }
+//   }
+// `;
