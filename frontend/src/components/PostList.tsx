498b1a714715143e87c4754a6f9094486a33254d
neginheidarii <neginheidari@icloud.com>
2025-07-23 13:18:39 -0400

    Implement PostList component for displaying fetched posts

---
+++ frontend/src/components/PostList.tsx
@@ -0,0 +1,25 @@
+import fetchPost from "@/services/fetchPost";
+
+const PostList = async () => {
+  const data = await fetchPost();
+  const posts = data.posts;
+
+  if (!data) return <p>No posts available.</p>;
+
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">All Posts</h1>
+      {posts?.map((post: any) => (
+        <div key={post.id} className="mb-4 border p-4 rounded">
+          <h2 className="text-lg font-semibold">{post.title}</h2>
+          <p>{post.content}</p>
+          <p className="text-sm text-gray-600">
+            Categories: {post.category?.map((cat: any) => cat.name).join(", ")}
+          </p>
+        </div>
+      ))}
+    </main>
+  );
+};
+
+export default PostList;
