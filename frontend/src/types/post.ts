ec9b049dffc368458c0fc5a95cd3b7e8bc6e8285
neginheidarii <neginheidari@icloud.com>
2025-07-23 16:22:09 -0400

    feat: add Category and Post types for GraphQL integration

---
+++ frontend/src/types/post.ts
@@ -0,0 +1,12 @@
+import { Category } from "./category";
+
+export type Post = {
+  id: string;
+  title: string;
+  content: string;
+  category: Category[]; 
+};
+
+export type PostsResponse = {
+  posts: Post[];
+};
