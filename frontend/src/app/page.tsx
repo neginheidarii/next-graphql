aad89c4a796d61beb224bae74819ea156ae23138
neginheidarii <neginheidari@icloud.com>
2025-07-23 13:18:35 -0400

    Replace Home component implementation with PostList for simplified rendering

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,45 +1,5 @@
-import fetchPost from "@/services/fetchPost";
-
-
-// Define the expected shape of data
-type Post = {
-  id: string;
-  title: string;
-  content: string;
-  category: {
-    name: string;
-  };
-};
-
-type PostsResponse = {
-  posts: Post[];
-};
+import PostList from "@/components/PostList";
 
 export default async function Home() {
-
-const data = (await fetchPost()) as PostsResponse;
-  const posts = data.posts;
-
-
-  if(!posts){
-    return <p>No post was found</p>
-  }
-
-
-
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">All Posts</h1>
-      {posts?.map((post:any) =>(
-        <div key={post.id} className="mb-4 border p-4 rounded">
-          <h2 className="text-lg font-semibold">{post.title}</h2>
-          <p>{post.content}</p>
-          <p className="text-sm text-gray-600">
-           Categories: {post.category?.map((cat: any) => cat.name).join(", ")}
-        </p>
-        </div>
-      ))}
-    </main>
-
-  );
+  return <PostList />;
 }
