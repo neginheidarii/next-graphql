9c82deb18a574ac5ea43a6981678fac729dc84e6
neginheidarii <neginheidari@icloud.com>
2025-07-23 16:21:42 -0400

    refactor: clean up layout.tsx by removing unnecessary blank lines

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-
-
+import "flowbite";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-  
-           {children}
-       
+        {children}
       </body>
     </html>
   );
