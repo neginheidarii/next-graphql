fca56843f392c67f0730a76bc00789db9144aa29
neginheidarii <neginheidari@icloud.com>
2025-07-24 13:51:10 -0400

    feat: implement Home component to fetch and display posts with Dropdown

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,20 @@
 import PostList from "@/components/PostList";
+import fetchPost from "@/services/fetchPosts";
+import Dropdown from "@/components/DropdownBtn";
 
 export default async function Home() {
-  return <PostList />;
+  const data = await fetchPost();
+  const posts = data.posts;
+
+  return (
+      <main className="p-6">
+      <h1 className="text-2xl font-bold mb-6">Blog Home</h1>
+      
+      <div className="mb-6">
+        <Dropdown />
+      </div>
+
+      <PostList posts={posts}/>
+    </main>)
+;
 }
