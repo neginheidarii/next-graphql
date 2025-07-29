5a7769a11fad8922d8844c80184dd49309461c5b
neginheidarii <neginheidari@icloud.com>
2025-07-29 14:04:44 -0400

    used button for filtering

--- frontend/src/components/DropdownMenu.tsx
+++ frontend/src/components/DropdownMenu.tsx
@@ -6,40 +6,38 @@ type Props = {
   categories: Category[];
   selectedCategoryId?: string;
   onClose: () => void;
+  onCategorySelect: (id: string) => void;
 };
 
 export default function DropdownMenu({
   categories,
   selectedCategoryId,
   onClose,
+  onCategorySelect,
 }: Props) {
   return (
     <div className="absolute mt-2 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
       <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
         <li>
-          <Link
-            href="/"
-            onClick={onClose}
-            className={`block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white ${
-              !selectedCategoryId ? "font-bold bg-gray-100 dark:bg-gray-600" : ""
-            }`}
+          <button
+            onClick={() => {
+              onCategorySelect("");
+              onClose();
+            }}
           >
             All Categories
-          </Link>
+          </button>
         </li>
         {categories.map((cat) => (
           <li key={cat.id}>
-            <Link
-              href={`/?category=${cat.id}`}
-              onClick={onClose}
-              className={`block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white ${
-                selectedCategoryId === cat.id
-                  ? "font-bold bg-gray-100 dark:bg-gray-600"
-                  : ""
-              }`}
+            <button
+              onClick={() => {
+                onCategorySelect(cat.id);
+                onClose();
+              }}
             >
               {cat.name}
-            </Link>
+            </button>
           </li>
         ))}
       </ul>
